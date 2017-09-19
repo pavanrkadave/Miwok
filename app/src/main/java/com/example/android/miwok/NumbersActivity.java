b68b1fb09011c875a17b3f58d0f17475bb1ac845
Pavan Hegde <pavanrkadave@gmail.com>
2017-09-19 18:40:21 +0530

    Changed the DataStructure to store numbers from Arrays to ArrayList<String>. also changed the way to display log messages

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -4,36 +4,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+
 public class NumbersActivity extends AppCompatActivity {
+
+    int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        //Array to store the Numbers
-        String[] words = new String[10];
-        words[0] = "One";
-        words[1] = "Two";
-        words[2] = "Three";
-        words[3] = "Four";
-        words[4] = "Five";
-        words[5] = "Six";
-        words[6] = "Seven";
-        words[7] = "Eight";
-        words[8] = "Nine";
-        words[9] = "Ten";
+        //ArrayList to store the Numbers
+        ArrayList<String> wordsList = new ArrayList<>();
+        wordsList.add("One");
+        wordsList.add("Two");
+        wordsList.add("Three");
+        wordsList.add("Four");
+        wordsList.add("Five");
+        wordsList.add("Six");
+        wordsList.add("Seven");
+        wordsList.add("Eight");
+        wordsList.add("Nine");
+        wordsList.add("Ten");
 
         //Log messages to display messages to console while the app is running.
-        Log.v("NumbersActivity : ", "Word At Index 0 => " + words[0]);
-        Log.v("NumbersActivity : ", "Word At Index 1 => " + words[1]);
-        Log.v("NumbersActivity : ", "Word At Index 2 => " + words[2]);
-        Log.v("NumbersActivity : ", "Word At Index 3 => " + words[3]);
-        Log.v("NumbersActivity : ", "Word At Index 4 => " + words[4]);
-        Log.v("NumbersActivity : ", "Word At Index 5 => " + words[5]);
-        Log.v("NumbersActivity : ", "Word At Index 6 => " + words[6]);
-        Log.v("NumbersActivity : ", "Word At Index 7 => " + words[7]);
-        Log.v("NumbersActivity : ", "Word At Index 8 => " + words[8]);
-        Log.v("NumbersActivity : ", "Word At Index 9 => " + words[9]);
+        for (String x : wordsList) {
+            Log.v("NumbersActivity : ", "Word at Position " + i + " =>" + x);
+            i++;
+        }
     }
 }
