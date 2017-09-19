e089f6f150d00700888605f36dd6dcbc63c7038e
Pavan Hegde <pavanrkadave@gmail.com>
2017-09-19 20:19:31 +0530

    Changed the views to ListView and ArrayAdapter to Recycle the views

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,7 +3,9 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -30,23 +32,15 @@ public class NumbersActivity extends AppCompatActivity {
         wordsList.add("Nine");
         wordsList.add("Ten");
 
-        //Find the rootView to add child views to it
-        LinearLayout rootView = (LinearLayout) findViewById(R.id.root_view);
+        //Using the ArrayAdapter to recycle the views
+        //This method makes efficient use of memory
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,wordsList);
 
+        //Identifying the ListView to put the words in the list
+        ListView listView = (ListView)findViewById(R.id.list);
 
-        //in for Loop the counter variable is initialized and
-        // incremented inside the for loop body itself
-        for (int index = 0; index < wordsList.size(); index++) {
-
-            //Create a new textView
-            TextView wordView = new TextView(this);
-
-            //Set the text to the word at the current index
-            wordView.setText(wordsList.get(index));
-
-            //Add the textView to the rootView
-            rootView.addView(wordView);
-        }
+        //Setting the itemsAdapter to the listView
+        listView.setAdapter(itemsAdapter);
 
     }
 }
