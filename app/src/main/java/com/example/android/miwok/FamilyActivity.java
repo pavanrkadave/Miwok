d4ae17434cbf7e20bd49f6cd09e2c56ef85141d1
Pavan Hegde <pavanrkadave@gmail.com>
2017-09-19 21:45:48 +0530

    Added the Code to PhrasesActivity So That it Works also added to family activity to make the code even in all branches

--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -2,12 +2,50 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class FamilyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_family);
+        setContentView(R.layout.activity_numbers);
+
+        /**
+         * ArrayList to store the FamilyMembers in the word class which is custom created by us.
+         * It holds both the english translation and the miwok translation
+         */
+        ArrayList<Word> words = new ArrayList<Word>();
+
+        //Adding the words to the words arrayList
+        words.add(new Word("One", "Lutti"));
+        words.add(new Word("Two", "Otiiko"));
+        words.add(new Word("Three", "Tolookosu"));
+        words.add(new Word("Four", "Oyyisa"));
+        words.add(new Word("Five", "Massokka"));
+        words.add(new Word("Six", "Temmokka"));
+        words.add(new Word("Seven", "Kenekaku"));
+        words.add(new Word("Eight", "Kawinta"));
+        words.add(new Word("Nine", "Wo’e"));
+        words.add(new Word("Ten", "Na’aacha"));
+
+        // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
+        // adapter knows how to create list items for each item in the list.
+        WordAdapter adapter = new WordAdapter(this, words);
+
+        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        // There should be a {@link ListView} with the view ID called list, which is declared in the
+        // activity_numbers.xml layout file.
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        //Setting the itemsAdapter to the listView
+        listView.setAdapter(adapter);
+
     }
 }
